d474b082aa143f2986e298679b2cbb4c2851a1a3
Nadav Wexler <nadavwe@wix.com>
2020-12-07 16:40:14 +0200

    another one

--- wix_build.sh
+++ wix_build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-GOPATH=/usr/local/go/ PATH=/usr/local/go/bin/:$PATH ARCH=amd64 OS=linux VERSION=1.2.3 ./build/build.sh
+GOROOT=/usr/local/go/ PATH=/usr/local/go/bin/:$PATH ARCH=amd64 OS=linux VERSION=1.2.3 ./build/build.sh
